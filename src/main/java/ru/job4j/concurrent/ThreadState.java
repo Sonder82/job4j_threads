4d4fac2a24dfd6e706d0dd8d934693c7674a3662
Sonder82 <Himrs82@gmail.com>
2022-11-17 12:11:17 +0500

    3. Состояние нити. [#229175 #361597]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -3,17 +3,17 @@ package ru.job4j.concurrent;
 public class ThreadState {
     public static void main(String[] args) {
         Thread first = new Thread(
-                () -> { }
+                () -> System.out.println("Hello " + Thread.currentThread().getName())
         );
         Thread second = new Thread(
-                () -> { }
+                () -> System.out.println("Hello " + Thread.currentThread().getName())
         );
         System.out.println("Состояние нити 1: " + first.getState());
         System.out.println("Состояние нити 2: " + second.getState());
         first.start();
         second.start();
         while (first.getState() != Thread.State.TERMINATED
-                && second.getState() != Thread.State.TERMINATED) {
+                || second.getState() != Thread.State.TERMINATED) {
             System.out.println("Состояние нити 1: " + first.getState());
             System.out.println("Состояние нити 2: " + second.getState());
         }
