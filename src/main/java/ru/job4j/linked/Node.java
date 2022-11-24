698baa2c41e417fb12b91d3cc813c785a00f4c37
Sonder82 <Himrs82@gmail.com>
2022-11-24 18:58:27 +0500

    3. Immutable объекты [#267918]

---
+++ src/main/java/ru/job4j/linked/Node.java
@@ -0,0 +1,24 @@
+package ru.job4j.linked;
+
+/**
+ * Класс описывает правила создания Immutable объекта.
+ * @param <T>
+ */
+public class Node<T> {
+
+    private final Node<T> next;
+    private final T value;
+
+    public Node(Node<T> next, T value) {
+        this.next = next;
+        this.value = value;
+    }
+
+    public Node<T> getNext() {
+        return next;
+    }
+
+    public T getValue() {
+        return value;
+    }
+}
