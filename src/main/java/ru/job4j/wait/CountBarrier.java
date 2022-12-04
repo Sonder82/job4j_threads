1b9e90a34c62df90edde849ac3d9f9d2b5c1418d
Sonder82 <Himrs82@gmail.com>
2022-12-04 16:11:47 +0500

    0. Управление нитью через wait. [#6858]

---
+++ src/main/java/ru/job4j/wait/CountBarrier.java
@@ -0,0 +1,52 @@
+package ru.job4j.wait;
+
+/**
+ * Класс выполняет блокирование по условию счетчика.
+ */
+public class CountBarrier {
+    /**
+     * Монитор
+     */
+    private final Object monitor = this;
+    /**
+     * содержит количество вызовов метода count().
+     */
+    private final int total;
+    /**
+     * счетчик
+     */
+    private int count = 0;
+
+    public CountBarrier(final int total) {
+        this.total = total;
+    }
+
+    /**
+     * Метод будет изменять состояние программы.
+     * Изменение состояния будет видно с помощью счетчика count.
+     * Метод notifyAll будет будить все нити, которые ждали изменения состояния.
+     */
+    public void count() {
+        synchronized (monitor) {
+            count++;
+            monitor.notifyAll();
+
+        }
+    }
+
+    /**
+     * Метод выполняет проверку количества изменения состояния программы.
+     * Метод wait будит все нити, которые ждали изменения состояния.
+     */
+    public void await() {
+        synchronized (monitor) {
+    while (count < total) {
+                try {
+                    monitor.wait();
+                } catch (InterruptedException e) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+        }
+    }
+}
